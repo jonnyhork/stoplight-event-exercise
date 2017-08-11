85cd729bc6640c13305772998e14cf8bc8e4cf83
Jonny Hork <JonnyHork@gmail.com>
2017-08-11 16:30:12 -0600

    buttons work

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 (function() {
   'use strict';
-
   // YOUR CODE HERE
+
+  console.log('hello');
+// STOP
+let stopButton = document.getElementById('stopButton')
+// console.log('stopButton: ', stopButton);
+let stopLight = document.getElementById('stopLight')
+
+let stop= stopButton.addEventListener('click', function(){
+  console.log('on');
+  stopLight.classList.toggle('stop')
+})
+
+// SLOW
+let slowButton = document.getElementById('slowButton')
+let slowLight = document.getElementById('slowLight')
+
+let slow = slowButton.addEventListener('click', function(){
+  // console.log('on');
+  slowLight.classList.toggle('slow')
+})
+
+// GO
+let goButton = document.getElementById('goButton')
+let goLight = document.getElementById('goLight')
+
+let go = goButton.addEventListener('click', function(){
+  // console.log('on');
+  goLight.classList.toggle('go')
+})
+
+
+
 })();
